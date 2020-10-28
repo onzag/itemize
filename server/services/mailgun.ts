81f87e7e67abc892af3f3d8ad06dea88542f1c6c
onzag <ejgonzalezviera@gmail.com>
2020-10-28 06:05:14 +0200

    added todos

--- server/services/mailgun.ts
+++ server/services/mailgun.ts
@@ -1,7 +1,8 @@
 import Mailgun from "mailgun-js";
 import { ISendEmailData, MailProvider } from ".";
 import type { Router } from "express";
-import { IAppDataType } from "../../server";
+import { IAppDataType } from "..";
+import { ITriggerRegistry } from "../resolvers/triggers";
 
 export class MailgunService extends MailProvider<Mailgun.ConstructorParams> {
   private mailgun: Mailgun.Mailgun;
@@ -17,6 +18,16 @@ export class MailgunService extends MailProvider<Mailgun.ConstructorParams> {
   public static async getRouter(appData: IAppDataType): Promise<Router> {
     // TODO setup a webhook if it's not there for the unsubscribe
     // TODO install the webhook and update the given property that represents the unsubscribe
+
+    return null;
+  }
+
+  public static async getTriggerRegistry(): Promise<ITriggerRegistry> {
+    // TODO when you update mail based properties and make them true
+    // we need to remove the user from the unsubscribe list given by
+    // mailgun blacklist
+
+    // so now we need to specify what these user properties are
 
     return null;
   }
