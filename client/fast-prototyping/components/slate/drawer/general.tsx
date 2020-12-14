fefabd107693bd08eaa29175b60e4ec85b6fbce8
onzag <ejgonzalezviera@gmail.com>
2020-12-14 12:06:08 +0200

    progress

--- client/fast-prototyping/components/slate/drawer/general.tsx
+++ client/fast-prototyping/components/slate/drawer/general.tsx
@@ -5,7 +5,9 @@ import {
   FormControl,
   IconButton,
   InputLabel,
-  NativeSelect,
+  Select,
+  FilledInput,
+  MenuItem,
 } from "../../../mui-core";
 import { IContainer } from "../../../../internal/text/serializer/container";
 import { ITitle } from "../../../../internal/text/serializer/title";
@@ -14,24 +16,40 @@ function GeneralContainerOptions(props: MaterialUISlateWrapperWithStyles) {
   const selectedNode: IContainer = props.info.currentSelectedNode as any;
   return (
     <div className={props.classes.box}>
-      <FormControl className={props.classes.selectionInput}>
-        <InputLabel htmlFor="slate-drawer-container-type-selector">Container Type</InputLabel>
-        <NativeSelect
-          value={selectedNode.containerType || ""}
-          className={props.classes.selectionInput}
-          inputProps={{
-            id: "slate-drawer-container-type-selector"
-          }}
+      <FormControl
+        variant="filled"
+        fullWidth={true}
+      >
+        <InputLabel
+          htmlFor="slate-drawer-container-type-selector"
+          shrink={true}
         >
-          <option value="">{" "}</option>
+          {props.i18nRichInfo.container}
+        </InputLabel>
+        <Select
+          value={selectedNode.containerType || ""}
+          displayEmpty={true}
+          variant="filled"
+          input={
+            <FilledInput
+              id="slate-drawer-container-type-selector"
+              placeholder={props.i18nRichInfo.container}
+            />
+          }
+        >
+          <MenuItem value="">
+            <em>{" "}</em>
+          </MenuItem>
           {
-            props.featureSupport.availableContainers.map((av) => {
-              return (
-                <option key={av.value} value={av.value}>{av.label}</option>
-              )
+            // render the valid values that we display and choose
+            props.featureSupport.availableContainers.map((vv) => {
+              // the i18n value from the i18n data
+              return <MenuItem key={vv.value} value={vv.value}>{
+                vv.label
+              }</MenuItem>;
             })
           }
-        </NativeSelect>
+        </Select>
       </FormControl>
     </div>
   );
@@ -41,22 +59,34 @@ function GeneralTitleOptions(props: MaterialUISlateWrapperWithStyles) {
   const selectedNode: ITitle = props.info.currentSelectedNode as any;
   return (
     <div className={props.classes.box}>
-      <FormControl className={props.classes.selectionInput}>
-        <InputLabel htmlFor="slate-drawer-title-type-selector">Title Type</InputLabel>
-        <NativeSelect
-          value={selectedNode.subtype || ""}
-          className={props.classes.selectionInput}
-          inputProps={{
-            id: "slate-drawer-title-type-selector"
-          }}
+      <FormControl
+        variant="filled"
+        fullWidth={true}
+      >
+        <InputLabel
+          htmlFor="slate-drawer-title-type-selector"
+          shrink={true}
         >
-          <option value="h1">h1</option>
-          <option value="h2">h2</option>
-          <option value="h3">h3</option>
-          <option value="h4">h4</option>
-          <option value="h5">h5</option>
-          <option value="h6">h6</option>
-        </NativeSelect>
+          {props.i18nRichInfo.title}
+        </InputLabel>
+        <Select
+          value={selectedNode.subtype || ""}
+          displayEmpty={true}
+          variant="filled"
+          input={
+            <FilledInput
+              id="slate-drawer-title-type-selector"
+              placeholder={props.i18nRichInfo.title}
+            />
+          }
+        >
+          <MenuItem value="h1">h1</MenuItem>
+          <MenuItem value="h2">h2</MenuItem>
+          <MenuItem value="h3">h3</MenuItem>
+          <MenuItem value="h4">h4</MenuItem>
+          <MenuItem value="h5">h5</MenuItem>
+          <MenuItem value="h6">h6</MenuItem>
+        </Select>
       </FormControl>
     </div>
   );
